24c49778387d4163f3987551b44a919d980ca7f6
Nigmacor <laevei@mail.ru>
2020-10-05 21:07:01 +0500

    отправка без торможения формы

--- chat/forms.py
+++ chat/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class ChatInputForm(forms.Form):
     chat = forms.CharField(label='', widget=forms.widgets.Input(attrs={'type':'text', 'class':'form-control', 'id':'chat-input', 'placeholder':'Введите новое сообщение...'}))
-    file = forms.FileField(label='', widget=forms.widgets.FileInput(attrs={'class':'form-control', 'id':'chat-file', 'multiple': 'multiple'}))
+    file = forms.FileField(required=False, label='', widget=forms.widgets.FileInput(attrs={'class':'form-control', 'id':'chat-file', 'multiple': 'multiple'}))
